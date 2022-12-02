411d047c1c9eafaf482f8834496421f8ccd6337a
mike <saintsikorski@gmail.com>
2022-12-02 18:38:33 +0100

    Get product info block
    Add getting StockX product information block, write content to text file.

--- spreadsheet-manipulation/main.py
+++ spreadsheet-manipulation/main.py
@@ -2,13 +2,14 @@
 
 # Imports
 
+from bs4 import BeautifulSoup
+
 import requests
 
 # Program variables
 
-url = 'https://stockx.com/nike-air-force-1-low-supreme-box-logo-white'
-size = 13
-productValue = 0
+base_url = 'https://stockx.com/nike-air-force-1-low-supreme-box-logo-white'
+base_size = 14
 
 # Request headers
 
@@ -29,9 +30,38 @@ headers = {
 
 # Program body
 
-def main():
-    r = requests.get(url, headers=headers)
-    print(r.text)
+def form_url():
+    url = base_url + '?size=' + str(base_size)
+    return url
 
+def request_page_content(url):
+    r = requests.get(url, headers=headers)
+    page_content = r.text
+    return page_content
+
+def process_page_content(html):
+    soup = BeautifulSoup(html, "html.parser")
+    find_pid = soup.find_all("div", {"class": "chakra-container css-vp2g1e"})
+
+    # Save product info
+    product_brand = ""
+    product_model = ""
+    product_sku = ""
+    product_price = ""
+
+    # Write content to file
+    f = open("content.txt", "w")
+    # ...
+
+    print(find_pid, file=f)
+
+    # Close the file
+    f.close()
+
+def main():
+    url = form_url() # Form request URL to the product page
+    html = request_page_content(url) # Send request to the URL and get page content
+    process_page_content(html)
+    
 if __name__ == '__main__':
     main()
