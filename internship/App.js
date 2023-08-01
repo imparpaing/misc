313022b7432679b1c65ffa44bb2b197438461491
mike <saintsikorski@gmail.com>
2023-08-01 14:14:12 +0200

    Get product out-of-warranty status

--- internship/App.js
+++ internship/App.js
@@ -6,13 +6,23 @@ async function main(serialNumber) {
     const headers = { 'Content-Type': 'application/json' };
     const url = 'https://pcsupport.lenovo.com/pl/pl/api/v4/upsell/redport/getIbaseInfo';
 
-    axios.post(url, requestBody, headers)
-        .then((response) => console.log(response.data))
-        .catch((error) => console.error(error.message));
+    try {
+        const response = await axios.post(url, requestBody, headers);
+        const outOfWarrantyStatus = response.data.data.oow;
+        console.log(`Product out of warranty: ${outOfWarrantyStatus}`);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 module.exports = main;
 
+/*******************************************
+ *                                         *
+ * REPLACE 'sn' WITH ACTIAL PRODUCT NUMBER *
+ *                                         *
+ *******************************************/
+
 if (require.main === module) {
-    main('');
+    main('sn');
 }
