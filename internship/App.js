00ab090e6e5adf6956125006a92df7be0c401738
mike <saintsikorski@gmail.com>
2023-08-02 14:32:32 +0200

    Get product name and warranty status

--- internship/App.js
+++ internship/App.js
@@ -8,8 +8,12 @@ async function main(serialNumber) {
 
     try {
         const response = await axios.post(url, requestBody, headers);
+        const productName = response.data.data.machineInfo.productName;
         const outOfWarrantyStatus = response.data.data.oow;
-        console.log(`Product out of warranty: ${outOfWarrantyStatus}`);
+        console.log(
+            "Product name: " + productName + "\n" +
+            "Product out of warranty: " + outOfWarrantyStatus
+        );
     } catch (error) {
         console.error(error.message);
     }
